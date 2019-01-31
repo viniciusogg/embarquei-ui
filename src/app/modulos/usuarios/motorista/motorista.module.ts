b55eec87d625d14495616d4294f0359278869bfe
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2019-01-31 06:22:24 -0300

    Criação do módulo do motorista

---
+++ src/app/modulos/usuarios/motorista/motorista.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PainelControleComponent } from './painel-controle/painel-controle.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PainelControleComponent]
+})
+export class MotoristaModule { }
